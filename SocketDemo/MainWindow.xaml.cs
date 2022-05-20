84d16923a71e7b3e95140029325a787f02bbb348
IWPFI <3141629907@qq.com>
2022-05-20 23:32:35 +0800

    连接服务端程序假死bug修复；添加多个连接
    1：解决没有连接服务端，窗体假死问题-->MainWindow.cs-43
    
    2：解决只有一个连接问题--实现多个连接

--- SocketDemo/MainWindow.xaml.cs
+++ SocketDemo/MainWindow.xaml.cs
@@ -1,19 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Net.Sockets;
-using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace SocketDemo
 {
@@ -39,16 +30,34 @@ namespace SocketDemo
             ShowMsg("监听成功");
             socketWatch.Listen(10);//设置服务端  时间点内最大容纳数
 
-            //等待客户端的连接 并且创建一个负责通信的Socket
-            Socket socketSend = socketWatch.Accept();
+            Thread th = new Thread(Listen);
+            th.IsBackground = true;//设置后台运行
+            th.Start(socketWatch);
+        }
 
-            //cmd连接服务端命令：telnet 172.28.112.1 50000
-            ShowMsg(socketSend.RemoteEndPoint.ToString() + ":" + "连接成功");
+        /// <summary>
+        /// 等待客户端的连接 并且创建一个负责通信的Socket
+        /// </summary>
+        private void Listen(object o)
+        {
+            Socket socketWatch = o as Socket; //线程只能传递一个object的参数,将传进来的参数进行装换
+
+            //等待客户端的连接 并且创建一个负责通信的Socket
+            while (true)
+            {
+                Socket socketSend = socketWatch.Accept();
+
+                //cmd连接服务端命令：telnet 172.28.112.1 50000
+                ShowMsg(socketSend.RemoteEndPoint.ToString() + ":" + "连接成功");
+            }
         }
 
         private void ShowMsg(string str)
         {
-            LogTextBox.AppendText(str + "\r\n");
+            this.LogTextBox.Dispatcher.Invoke(new Action(delegate
+            {
+                this.LogTextBox.AppendText(str + "\r\n");
+            }));
         }
     }
 }
