4c1c56a0f336b6549c34c334dd903d7c288d8c85
IWPFI <3141629907@qq.com>
2022-05-21 00:40:47 +0800

    将可能有异常的添加try{ } catch { }函数
    将可能有异常的添加try{ } catch { }函数，但不要抛出异常

--- SocketDemo/MainWindow.xaml.cs
+++ SocketDemo/MainWindow.xaml.cs
@@ -21,19 +21,23 @@ namespace SocketDemo
 
         private void StartButton_Click(object sender, RoutedEventArgs e)
         {
-            //当点击开始监听时 在服务器创建一个负责IP地址跟端口号的Socket
-            Socket socketWatch = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            IPAddress ip = IPAddress.Any;//IPAddress.Parse(serverTextBox.Text);
-            //创建端口号对象
-            IPEndPoint point = new IPEndPoint(ip, Convert.ToInt32(portTextBox.Text));
-            //监听
-            socketWatch.Bind(point);
-            ShowMsg("监听成功");
-            socketWatch.Listen(10);//设置服务端  时间点内最大容纳数
+            try
+            {
+                //当点击开始监听时 在服务器创建一个负责IP地址跟端口号的Socket
+                Socket socketWatch = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                IPAddress ip = IPAddress.Any;//IPAddress.Parse(serverTextBox.Text);
+                                             //创建端口号对象
+                IPEndPoint point = new IPEndPoint(ip, Convert.ToInt32(portTextBox.Text));
+                //监听
+                socketWatch.Bind(point);
+                ShowMsg("监听成功");
+                socketWatch.Listen(10);//设置服务端  时间点内最大容纳数
 
-            Thread th = new Thread(Listen);
-            th.IsBackground = true;//设置后台运行
-            th.Start(socketWatch);
+                Thread th = new Thread(Listen);
+                th.IsBackground = true;//设置后台运行
+                th.Start(socketWatch);
+            }
+            catch { }
         }
 
         /// <summary>
@@ -41,20 +45,24 @@ namespace SocketDemo
         /// </summary>
         private void Listen(object o)
         {
-            Socket socketWatch = o as Socket; //线程只能传递一个object的参数,将传进来的参数进行装换
-
-            //等待客户端的连接 并且创建一个负责通信的Socket
-            while (true)
+            try
             {
-                Socket socketSend = socketWatch.Accept();
+                Socket socketWatch = o as Socket; //线程只能传递一个object的参数,将传进来的参数进行装换
 
-                //cmd连接服务端命令：telnet 172.28.112.1 50000
-                ShowMsg(socketSend.RemoteEndPoint.ToString() + ":" + "连接成功");
+                //等待客户端的连接 并且创建一个负责通信的Socket
+                while (true)
+                {
+                    Socket socketSend = socketWatch.Accept();
 
-                Thread th = new Thread(Recive);
-                th.IsBackground = true;
-                th.Start(socketSend);
+                    //cmd连接服务端命令：telnet 172.28.112.1 50000
+                    ShowMsg(socketSend.RemoteEndPoint.ToString() + ":" + "连接成功");
+
+                    Thread th = new Thread(Recive);
+                    th.IsBackground = true;
+                    th.Start(socketSend);
+                }
             }
+            catch { }
         }
 
         /// <summary>
@@ -62,22 +70,26 @@ namespace SocketDemo
         /// </summary>
         private void Recive(object o)
         {
-            Socket socketSend = o as Socket;
-            while (true)
+            try
             {
-                //客户端连接成功后，服务器应该接收客户端发来的消息
-                byte[] buffer = new byte[1024 * 1024 * 2];//用来保存接收的数据--接收的是字节类型
-                //实际接收到的有效字节数
-                int r = socketSend.Receive(buffer);
-
-                if (r==0)
+                Socket socketSend = o as Socket;
+                while (true)
                 {
-                    break;
-                }
+                    //客户端连接成功后，服务器应该接收客户端发来的消息
+                    byte[] buffer = new byte[1024 * 1024 * 2];//用来保存接收的数据--接收的是字节类型
+                                                              //实际接收到的有效字节数
+                    int r = socketSend.Receive(buffer);
 
-                string str = Encoding.UTF8.GetString(buffer, 0, r);//转化成能读懂的字符串类型
-                ShowMsg(socketSend.RemoteEndPoint + ":" + str);
+                    if (r == 0)
+                    {
+                        break;
+                    }
+
+                    string str = Encoding.UTF8.GetString(buffer, 0, r);//转化成能读懂的字符串类型
+                    ShowMsg(socketSend.RemoteEndPoint + ":" + str);
+                }
             }
+            catch { }
         }
 
         private void ShowMsg(string str)
